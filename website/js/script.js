0740383ee61cedbc9f6dbca4ad2e9feb0118ac27
alucard822 <ars26.02.2008@gmail.com>
2025-01-26 07:16:20 +0500

    Остались небольшие моментики

--- website/js/script.js
+++ website/js/script.js
@@ -4,3 +4,23 @@ let burger = document.querySelector(".menu-dropbtn"),
 burger.addEventListener("click", function (e) {
   menu.classList.toggle("active");
 });
+
+/* Когда юзер нажимает появляется меню */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Меню закрывается, когда юзер кликает за меню
+window.onclick = function(event) {
+if (!event.target.matches('.button-menu')) {
+
+  var dropdowns = document.getElementsByClassName("dropdown-content");
+  var i;
+  for (i = 0; i < dropdowns.length; i++) {
+    var openDropdown = dropdowns[i];
+    if (openDropdown.classList.contains('show')) {
+      openDropdown.classList.remove('show');
+    }
+  }
+}
+}
