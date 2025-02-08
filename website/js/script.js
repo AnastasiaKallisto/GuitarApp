ab5ef2bd60bf2348080e71c440153bfc504878ba
alucard822 <ars26.02.2008@gmail.com>
2025-02-08 18:27:08 +0500

    Активный пункт меню

--- website/js/script.js
+++ website/js/script.js
@@ -1,36 +1,45 @@
 const dropdownMenu = document.getElementById("MainMenuDropdown");
 let hideTimeout; // Переменная для хранения идентификатора таймера
 
-function openMainMenu(){
+function openMainMenu() {
   dropdownMenu.classList.toggle("show");
 }
 
 dropdownMenu.addEventListener("mouseleave", () => {
-    // Устанавливаем таймер и сохраняем его ID
-    hideTimeout = setTimeout(() => {
-        dropdownMenu.classList.remove("show");
-    }, 1000);
+  // Устанавливаем таймер и сохраняем его ID
+  hideTimeout = setTimeout(() => {
+    dropdownMenu.classList.remove("show");
+  }, 1000);
 });
 
 dropdownMenu.addEventListener("mouseenter", () => {
-    // Если таймер активен — отменяем его
-    if (hideTimeout) {
-        clearTimeout(hideTimeout);
-        hideTimeout = null;
-    }
+  // Если таймер активен — отменяем его
+  if (hideTimeout) {
+    clearTimeout(hideTimeout);
+    hideTimeout = null;
+  }
 });
 
 // Меню закрывается, когда юзер кликает за меню
-window.onclick = function(event) {
-if (!event.target.matches('.main-menu-button')) {
-
-  var dropdowns = document.getElementsByClassName("dropdown-menu-content");
-  var i;
-  for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-      openDropdown.classList.remove('show');
+window.onclick = function (event) {
+  if (!event.target.matches(".main-menu-button")) {
+    var dropdowns = document.getElementsByClassName("dropdown-menu-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
     }
   }
-}
+};
+
+//Подсвечивает активный пункт меню
+const currentLocation = location.href;
+const menuItem = document.querySelectorAll('.MenuLink');
+const menuLenght = menuItem.length
+for (let i = 0; i<menuLenght; i++) {
+    if (menuItem[i].href === currentLocation) {
+        menuItem[i].className = "active MenuLink";
+    }
 }
