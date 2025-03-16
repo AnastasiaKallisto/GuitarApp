33292a2a828c06c4a29d305aab200d0ff8087f51
Anastasya Kallisto <anastasya.kallisto@gmail.com>
2025-03-16 19:31:00 +0600

    + Подключила swagger

--- GuitarApp/Program.cs
+++ GuitarApp/Program.cs
@@ -1,5 +1,20 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApiDocument();
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    // Add OpenAPI 3.0 document serving middleware
+    // Available at: http://localhost:<port>/swagger/v1/swagger.json
+    app.UseOpenApi();
+
+    // Add web UIs to interact with the document
+    // Available at: http://localhost:<port>/swagger
+    app.UseSwaggerUi();
+}
 
 app.MapGet("/", () => "Hello World!");
 
